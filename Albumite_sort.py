e18a8d52088e39dd879cc730e53f86d9f3238ab7
Susanna Kuusemaa <kuusemaa.susanna@gmail.com>
2020-06-26 13:31:35 +0300

    update

--- Albumite_sort.py
+++ Albumite_sort.py
@@ -15,6 +15,28 @@ def nimekiri_lugudest():
                 print()
         print(rida)
     fail.close()
+    
+def nimekiri_albumitest():
+    fail = open("albumid.txt", encoding='utf-8', errors ='ignore')
+    albumid = []
+    artistid = []
+    for rida in fail:
+        rida_elementide_kaupa = rida.split('\t')
+        artist = rida_elementide_kaupa[0]
+        artistid.append(artist)
+        album = rida_elementide_kaupa[1]
+        albumid.append(album)
+        aasta = rida_elementide_kaupa[2]
+        lugu = rida_elementide_kaupa[3]
+        if len(albumid) > 1:
+            if artistid[-2] != artistid[-1]:
+                print()
+                print(artist)
+                print("----------")
+                
+            if albumid[-2] != albumid[-1]:
+                print(album)
+    fail.close()
 # edasi teeme otsingu
 def otsing_albumid(otsitav_album):
     fail = open("albumid.txt", encoding='utf-8', errors ='ignore')
@@ -46,6 +68,9 @@ print()
 valik = int(input("Sisesta oma valik: "))
 if(valik == 1):
     nimekiri_lugudest()
+if (valik == 2):
+    print("Esitaja ja tema albumid on eraldatud kriipsudega!")
+    nimekiri_albumitest()
 if(valik == 3):
     otsitav_album = input("Millist albumit otsid: ")
     otsing_albumid(otsitav_album)
